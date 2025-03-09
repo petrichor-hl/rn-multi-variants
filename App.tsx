40bf68150518a3cca9969ecc8aebfc47550794b4
petrichor-hl <tl.hoanglam174@gmail.com>
2025-03-09 23:23:14 +0700

    feat: config env variables for iOS

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {SafeAreaView, Text, useColorScheme} from 'react-native';
-import DeviceInfo from 'react-native-device-info';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import DeviceInfo from 'react-native-device-info';
+import Config from 'react-native-config';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -12,7 +13,9 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <Text style={{fontSize: 28}}>{DeviceInfo.getBundleId()}</Text>
+      <Text style={{fontSize: 20}}>BUNDLE_ID = {DeviceInfo.getBundleId()}</Text>
+      <Text style={{fontSize: 20}}>ENV = {Config.ENV}</Text>
+      <Text style={{fontSize: 20}}>API_URL = {Config.API_URL}</Text>
     </SafeAreaView>
   );
 }
